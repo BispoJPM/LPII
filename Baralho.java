d55e8228fa6145d5248b14ac2ef4351affc197cb
João Pedro <142633184+BispoJPM@users.noreply.github.com>
2024-08-14 20:01:37 -0300

    Update Baralho.java

--- Baralho.java
+++ Baralho.java
@@ -50,5 +50,81 @@ public class Baralho {
 //     }
 // }
 
+// import java.util.Random;
+
+// public class Baralho {
+//     private Carta[] maço;
+//     private int topo; // Índice do topo do baralho
+
+//     public Baralho() {
+//         maço = new Carta[56];
+//         String[] naipes = {"Coração", "Espada", "Ouro", "Zap"};
+//         String[] nomes = {"As", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Dama", "Valete", "Rei"};
+
+//         int cartinha = 0;
+
+//         for (String naipe : naipes) {
+//             for (String nome : nomes) {
+//                 maço[cartinha++] = new Carta(nome, naipe);
+//             }
+//         }
+//         for (int crg = 0; crg < 4; crg++) {
+//             maço[cartinha++] = new Carta("Coringa", "Coringa");
+//         }
+
+//         topo = 55; // Define o topo como o índice da última carta
+
+//         embaralha(10); // Embaralha o baralho 10 vezes, conforme especificado
+//     }
+
+//     // Método para embaralhar o baralho 'total' vezes
+//     private void embaralha(int total) {
+//         Random random = new Random();
+//         for (int x = 0; x < total; x++) {
+//             for (int emb = maço.length - 1; emb > 0; emb--) {
+//                 int sid = random.nextInt(emb + 1);
+//                 Carta subs = maço[emb];
+//                 maço[emb] = maço[sid];
+//                 maço[sid] = subs;
+//             }
+//         }
+//     }
+
+//     // Método para distribuir uma carta do topo do baralho
+//     public Carta daCarta() {
+//         if (temCarta()) {
+//             return maço[topo--]; // Retorna a carta do topo e decrementa o índice do topo
+//         } else {
+//             return null; // Retorna null se não houver mais cartas
+//         }
+//     }
+
+//     // Método para verificar se ainda há cartas no baralho
+//     public boolean temCarta() {
+//         return topo >= 0;
+//     }
+
+//     // Método para imprimir as cartas do baralho
+//     public void imprimeBaralho() {
+//         for (int i = 0; i <= topo; i++) {
+//             System.out.println(maço[i]);
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Baralho baralho = new Baralho();
+
+//         System.out.println("Baralho embaralhado:");
+//         baralho.imprimeBaralho();
+
+//         System.out.println("\nDistribuindo cartas:");
+//         while (baralho.temCarta()) {
+//             System.out.println("Carta retirada: " + baralho.daCarta());
+//         }
+
+//         System.out.println("\nAinda há cartas? " + baralho.temCarta());
+//     }
+// }
+
 
 
